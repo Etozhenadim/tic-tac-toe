04cc91ec9d68a12f062c7034dd5f437bc039494f
n.iskakov <n.iskakov@platform.live>
2022-09-19 17:28:34 +0200

    created game

--- src/components/game.js
+++ src/components/game.js
@@ -17,4 +17,6 @@ export class Game extends React.Component {
       );
     }
   }
+
+
   
--- src/components/square.js
+++ src/components/square.js
@@ -1,19 +1,10 @@
 import React from "react";
 
-export class Square extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        value: null,
-      };
-    }
-
-    render() {
+export function Square(props) {
       return (
-        <button className="square" 
-                onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
-    }
   }
+
